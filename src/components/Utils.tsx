0e618f04d7f16a64a50c0c15cfa08b90b19ef3b4
Luvjeet <luzag1402@gmail.com>
2023-08-18 17:07:34 +0530

    feat: host it please!

--- src/components/Utils.tsx
+++ src/components/Utils.tsx
@@ -5,11 +5,17 @@ export function getNeighbours(
     currNode: BoardTypeNode,
 ): BoardTypeNode[] {
     const neighbors: BoardTypeNode[] = [];
+    // @ts-ignore
     if (currNode.col < grid[0].length - 1)
+        // @ts-ignore
         neighbors.push(grid[currNode.row][currNode.col + 1]);
+    // @ts-ignore
     if (currNode.row < grid.length - 1)
+        // @ts-ignore
         neighbors.push(grid[currNode.row + 1][currNode.col]);
+    // @ts-ignore
     if (currNode.row > 0) neighbors.push(grid[currNode.row - 1][currNode.col]);
+    // @ts-ignore
     if (currNode.col > 0) neighbors.push(grid[currNode.row][currNode.col - 1]);
 
     return neighbors.filter((nbr) => !nbr.isWall && !nbr.isVisited);
@@ -20,6 +26,7 @@ export function getShortestPath(target: BoardTypeNode): BoardTypeNode[] {
     let curr = target;
     while (curr) {
         temp.unshift(curr);
+        // @ts-ignore
         curr = curr.previousNode;
     }
     return temp;
